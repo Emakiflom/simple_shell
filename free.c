5d2fa3cd7491c338e81c281f2122a59508c247df
Emakiflom <amankflom23@gmail.com>
2023-11-14 23:56:34 -0800

    free.c

--- free.c
+++ free.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * free_all - This function is designed to free dynamically allocated memory at
-              the end of the main loop in your shell program.
+ * free_all - This function is designed to free dynamically allocated memory
+ * atthe end of the main loop in your shell program.
  * The function uses the free function to release the memory occupied
-             by each dynamically allocated variable.
+ * by each dynamically allocated variable.
  * It checks whether fullpath was dynamically allocated
-             (flag == 1) before attempting to free it.
+ * (flag == 1) before attempting to free it.
  * @tokens: Pointer to tokens array.
  * @path: Pointer to path variable.
  * @line: Pointer to user input buffer.
@@ -16,38 +16,38 @@
  */
 void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
 {
-    /*  Free dynamically allocated path variable */
-    free(path);
-    /* Free dynamically allocated tokens array */
-    free(tokens);
-    /* Free dynamically allocated user input buffer */
-    free(line);
-    /* Check if fullpath was dynamically allocated before freeing */
-    if (flag == 1)
-        free(fullpath);
+	/*  Free dynamically allocated path variable */
+	free(path);
+	/* Free dynamically allocated tokens array */
+	free(tokens);
+	/* Free dynamically allocated user input buffer */
+	free(line);
+	/* Check if fullpath was dynamically allocated before freeing */
+	if (flag == 1)
+		free(fullpath);
 }
 
 /**
- * free_dp - This function is responsible for freeing a double pointer (array) and the memory
-              occupied by the strings it points to.
- * The function uses a for loop to iterate through each element of the double pointer.
+ * free_dp - This function is responsible for freeing a double pointer (array)
+ * and the memoryoccupied by the strings it points to.
+ * The function uses for loop to iterate each element of the double pointer.
  * It uses the free function to release the memory occupied by the strings
-              pointed to by the double pointer.
- * Finally, it frees the memory occupied by the double pointer itself using free(array).
+ * pointed to by the double pointer.
+ * Finally, it frees the memory occupied by the double pointer
+ * itself using free(array).
  * @array: Double pointer to free.
  * @length: Length of the double pointer.
  * Return: void
  */
 void free_dp(char **array, unsigned int length)
 {
-    unsigned int i;
-
-    /* Loop through each element of the double pointer */
-    for (i = 0; i < length; i++)
-    {
-        /* Free the memory occupied by the strings pointed to by the double pointer */
-        free(array[i]);
-    }
-    /* Free the memory occupied by the double pointer itself*/
-    free(array);
+	unsigned int i;
+	/* Loop through each element of the double pointer */
+	for (i = 0; i < length; i++)
+	{
+		/* Free the memory occupied by the strings pointed to by double pointer */
+		free(array[i]);
+	}
+	/* Free the memory occupied by the double pointer itself*/
+	free(array);
 }
