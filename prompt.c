694c2850b3e8b699b2beccc37c1178039b6e6043
Makitey <weldemichaelmakda@gmail.com>
2023-11-15 12:03:59 -0800

    prompt.c

--- prompt.c
+++ prompt.c
@@ -2,28 +2,31 @@
 
 /**
  * prompt - This function checks whether the shell is in interactive mode by
-             examining the file descriptor's information. If it's in interactive
-			 mode (determined by checking if the file is a character special file),
-			 it prints the shell prompt.
+ * examining the file descriptor's information. If it's in interactive mode
+ * (determined by checking if the file is a character special file),
+ * it prints the shell prompt.
  * The fd parameter represents the file descriptor associated with the file,
-         and buf is a structure containing information about the file, obtained using the fstat function.
+ * and buf is a structure containing information about the file, obtained
+ * using the fstat function.
  * if (S_ISCHR(buf.st_mode)): Checks if the file is a character special file
-         (typically a terminal) by using the S_ISCHR macro.
+ * (typically a terminal) by using the S_ISCHR macro.
  * @fd: File stream (file descriptor).
  * @buf: Buffer containing information about the file.
  */
 void prompt(int fd, struct stat buf)
 {
-    /* Use the fstat function to get information about the file associated with the file descriptor (fd). */
-    fstat(fd, &buf);
-
-    /* Check if the file is a character special file (character device, like a terminal). */
-    if (S_ISCHR(buf.st_mode))
-        _puts(PROMPT); /* If it's a character special file, print the shell prompt. */
+	/* Use the fstat function to get information about the file */
+	/*  associated with the file descriptor (fd). */
+	fstat(fd, &buf);
+	/* Check if the file is a character special file */
+	/*  (character device, like a terminal). */
+	if (S_ISCHR(buf.st_mode))
+		_puts(PROMPT); /* If it's character special file print shell */
 }
 
 /**
- * _puts -  function to print the shell prompt (defined elsewhere in the code as PROMPT).
+ * _puts -  function to print the shell prompt (defined elsewhere
+ * in the code as PROMPT).
  * Calculates the length of the input string using str_len.
  * Uses the write function to print the string to the standard output.
  * @str: String to print.
@@ -31,11 +34,10 @@ void prompt(int fd, struct stat buf)
  */
 void _puts(char *str)
 {
-    unsigned int length;
-
-    /* Calculate the length of the input string. */
-    length = str_len(str);
-
-    /* Use the write function to print the string to the standard output (STDOUT_FILENO). */
-    write(STDOUT_FILENO, str, length);
+	unsigned int length;
+	/* Calculate the length of the input string. */
+	length = str_len(str);
+	/* Use the write function to print the string to */
+	/* the standard output (STDOUT_FILENO). */
+	write(STDOUT_FILENO, str, length);
 }
