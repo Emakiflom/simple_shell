c257d331cdb40e07a4a207f8a4a98349f87270dc
Emakiflom <amankflom23@gmail.com>
2023-11-15 00:40:46 -0800

    get_line.c

--- get_line.c
+++ get_line.c
@@ -1,39 +1,39 @@
 #include "shell.h"
 
 /**
- * get_line - This function is responsible for reading a line of input from the user.
- * It takes a file stream (fp) as an argument, which allows it to read from either
-             stdin or a file, depending on usage.
- * line is a pointer that will point to the buffer where the user input is stored.
+ * get_line - This function is responsible for reading line of input from user.
+ * It takes a file stream (fp) as an argument, which allows it to read from
+ * eitherstdin or a file, depending on usage.
+ * line is a pointer that will point to buffer where the user input is stored.
  * read is a variable that holds the number of characters read. It is of
-              type ssize_t, which is a signed size type.
+ * type ssize_t, which is a signed size type.
  * len is the length of the buffer. It starts with an initial value of 0.
- * The getline function is used to read a line from the file stream (fp).
-            It dynamically allocates memory for line and updates len with the size of the allocated buffer.
- * If getline encounters an error or reaches the end of the file, it returns -1, and the
-             function frees the memory allocated for line and exits the program with a success status.
- * If successful, the function returns the buffer line containing the user input.
+ * The getline function is used to read a line from the file stream (fp).It
+ * dynamically allocates memory for line and updates
+ * len with the size of the allocated buffer.
+ * If getline encounters an error or reaches the end of the file,
+ * it returns -1, and thefunction frees the memory allocated for
+ * line and exits the program with a success status.
+ * If successful, the function returns the buffer line containing user input.
  * @fp: File stream (stdin or a file, depending on usage).
  * Return: The buffer containing user input.
  */
+
 char *get_line(FILE *fp)
 {
-    char *line;         /* Pointer to the buffer for user input*/
-    ssize_t read;       /* Number of characters read, signed size type */
-    size_t len;         /* Length of the buffer */
+	char *line;         /* Pointer to the buffer for user input*/
+	ssize_t read;       /* Number of characters read, signed size type */
+	size_t len;         /* Length of the buffer */
 
-    line = NULL;        /* Initialize line to NULL to avoid undefined behavior */
-    len = 0;            /* Initialize len to 0 */
-
-    /* Read a line of input from the user into the buffer 'line' */
-    read = getline(&line, &len, fp);
-
-    /* Check if getline encountered an error or reached the end of the file */
-    if (read == -1)
-    {
-        free(line);     /* Free the memory allocated for the buffer */
-        exit(EXIT_SUCCESS); /* Exit the program with success status */
-    }
-
-    return (line);      /* Return the buffer containing the user input */
+	line = NULL;  /* Initialize line to NULL to avoid undefined behavior */
+	len = 0;            /* Initialize len to 0 */
+	/* Read a line of input from the user into the buffer 'line' */
+	read = getline(&line, &len, fp);
+	/* Check if getline encountered an error or reached the end of file */
+	if (read == -1)
+	{
+		free(line);     /* Free the memory allocated for the buffer */
+		exit(EXIT_SUCCESS); /* Exit the program with success status */
+	}
+	return (line);      /* Return the buffer containing the user input */
 }
